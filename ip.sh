118b6eb1cf705b5e10916a97064ff3593d157f83
Eurgen <39940697+Eurgen@users.noreply.github.com>
2024-10-17 22:01:36 +0300

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -55,12 +55,12 @@ endpoint4() {
 	n=0
 	iplist=100
 	while true; do
-		temp[$n]=$(echo 162.159.190.$(($RANDOM % 256)))
+		temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
 		n=$(($n + 1))
 		if [ $n -ge $iplist ]; then
 			break
 		fi
-		temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
+		temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
 		n=$(($n + 1))
 		if [ $n -ge $iplist ]; then
 			break
